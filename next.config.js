e2212e07aac703996edbe96f0e0837f8f15b3193
Öner Zafer <onerzfr@gmail.com>
2023-06-11 23:07:36 +0300

    console verification trial 2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: '/html/googlee73412404e862c11.html',
+        destination: '/googlee73412404e862c11.html',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
